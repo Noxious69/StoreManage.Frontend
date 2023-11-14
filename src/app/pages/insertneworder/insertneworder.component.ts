e003923e86670b4fbe22ea87ed994db9cee1dd34
ali <shabaisuorosh@gmail.com>
2023-11-14 22:33:47 +0330

    import func

--- src/app/pages/insertneworder/insertneworder.component.ts
+++ src/app/pages/insertneworder/insertneworder.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable, map, startWith } from 'rxjs';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BackendneworderService } from '../services/backendneworder.service';
 
 @Component({
   selector: 'app-insertneworder',
@@ -11,7 +13,7 @@ import { FormBuilder } from '@angular/forms';
 export class InsertneworderComponent {
   dateValue = new FormControl();
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder , private backend:BackendneworderService , private router:Router) {}
 
   order = this.fb.group({
     ordertype: [''],
@@ -27,6 +29,31 @@ export class InsertneworderComponent {
     customerphone: [''],
     date: [this.dateValue],
   });
+
+public import(){
+
+  let ordertype : string | undefined = this.order.controls.ordertype.value?.toString();
+  let ordercolor : string | undefined = this.order.controls.ordercolor.value?.toString();
+  let ordercount : number | undefined = Number(this.order.controls.ordercount.value?.toString());
+
+  let boxcount : number | undefined = Number(this.order.controls.boxcount.value?.toString());
+  let boxtype : string | undefined = this.order.controls.boxtype.value?.toString();
+
+  let customername : string | undefined = this.order.controls.customername.value?.toString();
+  let customerphone : string | undefined = this.order.controls.customerphone.value?.toString();
+  let customeraddress : string | undefined = this.order.controls.customeraddress.value?.toString();
+
+  let edge : boolean | undefined = Boolean(this.order.controls.edge.value?.toString());
+  let twocolor : boolean | undefined = Boolean(this.order.controls.twocolor.value?.toString());
+  let lable : boolean | undefined = Boolean(this.order.controls.lable.value?.toString());
+
+  this.backend.newOrder(ordertype??'' , ordercolor??'', ordercount??0 , boxcount??0 , boxtype??'' , customername??'' , customerphone??'' , customeraddress??'' , edge??false , twocolor??false , lable??false ).subscribe(r=>
+    { 
+      this.router.navigate(['/newtask'])
+    });
+}
+  
+  
 
   keyNumber(event: KeyboardEvent) {
     const pattern = /[0-9]/;
--- src/app/pages/services/backendneworder.service.ts
+++ src/app/pages/services/backendneworder.service.ts
@@ -6,10 +6,21 @@ import { BackendService } from './backend.service';
 })
 export class BackendneworderService extends BackendService{
 
-  newOrder(){
+  newOrder(ordertype:string , ordercolor:string , ordercount:number , boxcount:number , boxtype : string , customername :string , customerphone :string , customeraddress : string , edge : boolean , twocolor : boolean , lable : boolean ){
     return this.http.post(this.securityAPI + 'newOrder' , 
     {
-      
+      ordertype : ordertype ,
+      ordercolor : ordercolor,
+      ordercount : ordercount ,
+      boxcount: boxcount ,
+      boxtype : boxtype ,
+      customername : customername,
+      customerphone : customerphone,
+      customeraddress : customeraddress,
+      edge : edge,
+      twocolor : twocolor,
+      lable : lable,
+
     })
   }
 }
